527fd8f5553c3e1aca076df6ea0c79a612ab2780
ethandietrich00808 <edietri2@vols.utk.edu>
2025-02-27 22:32:22 -0500

    change host ip

--- app.py
+++ app.py
@@ -94,4 +94,4 @@ def update_map(selected_degree_level, selected_skill):
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True) 
+    app.run_server(host='0.0.0.0', port=8050, debug=True)
